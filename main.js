d3e9393e65c31cac1a6dd3986b1745b1f6c7c37c
Emiluz <emiluzlopez@yahoo.com>
2015-06-02 20:32:16 -0700

    message recieved, over and out

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 alert("i'm working with JavaScript");
+function mad_libs() {
+	document.write('This is the story of a girl that was lost in all this different languages. Sometimes she sees the light, sometimes is all darkness... What she should do go to the light or run away... ');
+};
