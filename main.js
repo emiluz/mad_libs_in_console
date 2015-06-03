8167fa31a92ca5714af75caca7c98ea4be8f8a80
Emiluz <emiluzlopez@yahoo.com>
2015-06-03 07:33:09 -0700

    fixed text

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function mad_libs() {
 	var verb = ["lost", "sees", "is", "was"];
 	var preposition = ["of", "in"];
 	document.write('This ' + verb[2] +' '+ article[0] +' '+ noun[1] +' '+ preposition[0]+' '+ article[1]+' '+ noun[0] +
-	'that '  + verb [3]+' '+ verb [0]+' '+ preposition [1]+' '+ 'all these different' +' '+ noun [2] + '. Sometimes she' 
+	' that '  + verb [3]+' '+ verb [0]+' '+ preposition [1]+' '+ 'all these different' +' '+ noun [2] + '. Sometimes she' 
 	+' '+ verb[1]+' '+ article[0]+' '+ noun[3] + ', sometimes ' + verb[2] + ' all darkness... What should she do? Go to' 
 	+' '+ article[0]+' '+ noun[3] + ' or run away?');
 	
