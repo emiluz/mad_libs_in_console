8a7c117ab0f1ab6a8a03aca0cd9abdd0dc456ab4
Emiluz <emiluzlopez@yahoo.com>
2015-06-02 23:01:30 -0700

    mad-libs mission accomplished

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 alert("i'm working with JavaScript");
 function mad_libs() {
-	document.write('This is the story of a girl that was lost in all this different languages. Sometimes she sees the light, sometimes is all darkness... What she should do go to the light or run away... ');
+	var noun = ["girl", "story", "languages", "light"];
+	var article = ["the", "a"];
+	var verb = ["lost", "sees", "is", "was"];
+	var preposition = ["of", "in"];
+	document.write('This ' + verb[2] +' '+ article[0] +' '+ noun[1] +' '+ preposition[0]+' '+ article[1]+' '+ noun[0] +
+	'that '  + verb [3]+' '+ verb [0]+' '+ preposition [1]+' '+ 'all these different' +' '+ noun [2] + '. Sometimes she' 
+	+' '+ verb[1]+' '+ article[0]+' '+ noun[3] + ', sometimes ' + verb[2] + ' all darkness... What should she do? Go to' 
+	+' '+ article[0]+' '+ noun[3] + ' or run away?');
+	
 };
+
+mad_libs();
+
